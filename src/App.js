6f4906203c645c3f85f5fd56bb54dcbf39ba3d03
ximnoise <mrogge96@protonmail.com>
2021-01-28 20:01:45 +0100

    add citysearch + mockdata + corresponding tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-import './EventList';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CitySearch from '../CitySearch';
+
+describe('<CitySearch /> component', () => {
+  let CitySearchWrapper;
+  beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
+
+  test('render text input', () => {
+    expect(CitySearchWrapper.find('.city')).toHaveLength(1);
+  });
+
+  test('renders a list of suggestions', () => {
+    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  });
+
+  test('renders text input correctly', () => {
+    const query = CitySearchWrapper.state('query');
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+  });
+
+  test('change state when text input changes', () => {
+    CitySearchWrapper.setState({
+      query: 'Munich'
+    });
+    const eventObject = { target: { value: 'Berlin'}};
+    CitySearchWrapper.find('.city').simulate('change', eventObject);
+    expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  });
+
+}); 
