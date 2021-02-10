a6c40a0bcb4cccaf6380115a4edc98600fb8b360
ximnoise <mrogge96@protonmail.com>
2021-02-10 12:06:34 +0100

    add puppeteer launch parameter

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,7 +6,11 @@ describe('show/hide an event details', () => {
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+        //headless: false,
+        //slowMo: 250,
+        //ignoreDefaultArgs: ['--disable-extensions']
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
