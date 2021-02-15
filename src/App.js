8f200c1444c0fb3e0491ab81e5d73feb3f68f6c3
ximnoise <mrogge96@protonmail.com>
2021-02-15 15:12:51 +0100

    bugfix loginView 179bb7a

--- src/App.js
+++ src/App.js
@@ -19,13 +19,17 @@ class App extends Component {
 
   async componentDidMount() {
     const accessToken = localStorage.getItem('access_token');
-    const tokenCheck = accessToken !== null ? checkToken(accessToken) : false;
-    this.setState({ tokenCheck });
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get('code');
-    this.mounted = true;
-    if (code && this.mounted === true) {
-      this.setState({ tokenCheck: true }, this.updateEvents());
+    const validToken = accessToken !== null ? await checkToken(accessToken) : false;
+    this.setState({ tokenCheck: validToken });
+    if (validToken === true) {
+      this.updateEvents()
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = searchParams.get('code');
+      this.mounted = true;
+      if (code && this.mounted === true && validToken === false) {
+        this.setState({ tokenCheck: true });
+        this.updateEvents();
+      }
     }
 
     getEvents().then((response) => {
