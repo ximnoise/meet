7996f26e02ad8605f9e1c0a7b71df0437c569bbf
ximnoise <mrogge96@protonmail.com>
2021-02-15 17:11:14 +0100

    add update styling login page

--- src/Login.js
+++ src/Login.js
@@ -7,19 +7,23 @@ function Login() {
       <h4>
         Log in to see upcoming events around the world for full-stack developers
       </h4>
-      <div className='btn-google'>
-        <a
-          className='btn-login'
-          href='https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&response_type=code&client_id=599445530558-r804ljdavn98l072h16mrqdi22lv9q48.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fximnoise.github.io%2Fmeet'
-          rel='nofollow noopener'
-        >
-          <img
-            className='google-icon'
-            src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-            alt='Google sign-in'
-          />
-          <b>Sign in with google</b>
-        </a>
+      <div className='button_cont' align='center'>
+        <div className='google-btn'>
+          <div className='google-icon-wrapper'>
+            <img
+              className='google-icon'
+              src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+              alt='Google sign-in'
+            />
+          </div>
+          <a
+            className='btn-text'
+            href='https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&response_type=code&client_id=599445530558-r804ljdavn98l072h16mrqdi22lv9q48.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fximnoise.github.io%2Fmeet'
+            rel='nofollow noopener'
+          >
+            <b>Sign in with google</b>
+          </a>
+        </div>
       </div>
       <a
         className='btn-privacy'
@@ -31,4 +35,6 @@ function Login() {
     </div>
   );
 }
+
+//
 export default Login;
