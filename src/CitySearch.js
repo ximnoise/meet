489ac1025a937f7ae8d5f83144a6147fa9d32660
ximnoise <mrogge96@protonmail.com>
2021-02-16 12:32:46 +0100

    add alert.js + funtionality

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -13,6 +14,7 @@ class CitySearch extends Component {
       query: suggestion,
       suggestions: [],
       showSuggestions: false,
+      infoText: ''
     });
     this.props.updateEvents(suggestion);
   };
@@ -25,10 +27,18 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city.'
+      });
+    } else {
+      this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   render() {
@@ -36,6 +46,7 @@ class CitySearch extends Component {
 
     return (
       <div className='CitySearch'>
+        <InfoAlert text={this.state.infoText} />
         <label>Choose your nearest city</label>
         <input
           type='text'
