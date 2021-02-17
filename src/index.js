d7633ee95812942d35414e634c1815c6b1142809
ximnoise <mrogge96@protonmail.com>
2021-02-17 09:51:19 +0100

    add manifest.json + app icons + serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 atatus.config('aed2091d51704321bb224f67207390e8').install();
+
+serviceWorkerRegistration.register();
